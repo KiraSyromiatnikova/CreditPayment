f13864f2f57a351367cbd52f97ae93a2d3863d3d
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-01-26 01:39:15 +0300

    CreditPaymentUPD

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public float calculate(float rate_year, float total, float period_month) {
+    public float calculate(float rate_year, int total, int period_month) {
         double dblResult = Math.pow(rate_year/12/100+1, period_month);
         double payment = total * rate_year/12/100 * dblResult / (dblResult - 1);
         return (long) payment;
