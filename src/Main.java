9969da8c7104b301cb458e6ce921c27a42d897d6
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-01-26 22:05:04 +0300

    Reformat Code

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
         CreditPaymentService service = new CreditPaymentService();
         long payment = (long) service.calculate((float) 9.99, 1000000, 36);
-       System.out.println(payment);
+        System.out.println(payment);
     }
 }
