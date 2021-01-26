20f9aca7fa21604d93537f77a21517ed8c94b619
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-01-27 01:28:37 +0300

    camelCase

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public float calculate(float rate_year, int total, int period_month) {
-        double dblResult = Math.pow(rate_year / 12 / 100 + 1, period_month);
-        double payment = total * rate_year / 12 / 100 * dblResult / (dblResult - 1);
+    public float calculate(float rateYear, int total, int periodMonth) {
+        double dblResult = Math.pow(rateYear / 12 / 100 + 1, periodMonth);
+        double payment = total * rateYear / 12 / 100 * dblResult / (dblResult - 1);
         return (long) payment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         CreditPaymentService service = new CreditPaymentService();
-        long payment = (long) service.calculate((float) 9.99, 1000000, 36);
+        long payment = (long) service.calculate((float) 9.99, 1000000, 12);
         System.out.println(payment);
     }
 }
